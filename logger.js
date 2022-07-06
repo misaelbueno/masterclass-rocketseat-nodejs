134f53339f56a7c2777846c3309ad887da7935cf
misael.bueno <misael.bueno@meta.com.br>
2022-07-06 16:09:36 -0300

    end logger

---
+++ logger.js
@@ -0,0 +1,17 @@
+ const eventEmitter = require('events')
+ const fs = require('fs')
+ const path = require('path')
+
+ const emitter = new eventEmitter()
+
+ emitter.on('log', (message) => {
+    fs.appendFile(path.join(__dirname, 'log.txt'), message, err => {
+        if (err) throw err
+    })
+ })
+
+function log(message) {
+    emitter.emit('log', message)
+}
+
+module.exports = log;
--- stats.js
+++ stats.js
@@ -1,4 +1,5 @@
 const os = require('os')
+const log = require('./logger')
 
 setInterval(() => {
     const { freemem, totalmem } = os
@@ -15,5 +16,7 @@ setInterval(() => {
 
     console.clear();
     console.table(stats);
+
+    log(`${JSON.stringify(stats)}\n`)
 
 }, 1000)
